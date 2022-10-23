0a6cefe18fb22749bedf7f15a243d6cda96532c9
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-23 14:21:29 +0600

    connect database and its collection

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-var mongoDb = require('mongodb').MongoClient;
+// var mongoDb = require('mongodb').MongoClient;
 
-var url = 'mongodb+srv://ashraf:01682244160ok@cluster0.cdo9y27.mongodb.net/?retryWrites=true&w=majority';
+// var url = 'mongodb+srv://ashraf:01682244160ok@cluster0.cdo9y27.mongodb.net/?retryWrites=true&w=majority';
 
-mongoDb.connect(url, function (error) {
-    if (error) {
-        console.log("Failed Connection");
+// mongoDb.connect(url, function (error) {
+//     if (error) {
+//         console.log("Failed Connection");
+//     }
+//     else {
+//         console.log("Connected");
+//     }
+// });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ashraf:01682244160ok@cluster0.cdo9y27.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("school").collection("students");
+    // perform actions on the collection object
+    client.close();
+    if(err) {
+        console.log("Failed Connection"); 
     }
     else {
         console.log("Connected");
     }
+ 
 });
