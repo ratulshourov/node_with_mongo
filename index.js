1fa9092b1730d42c8637944e8d9628bb300972c7
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-25 10:21:20 +0600

    header and cookie work

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var mongoDb = require('mongodb').MongoClient;
-var express=require('express');
+var express = require('express');
 
-var app=express();
+var app = express();
 //var url = 'mongodb+srv://ashraf:01682244160ok@cluster0.cdo9y27.mongodb.net/?retryWrites=true&w=majority';
 var url = 'mongodb://localhost:27017/';
-var config={ useUnifiedTopology:true };
+var config = { useUnifiedTopology: true };
 
-app.get('/',function(req,response)
-{
+app.get('/', function (req, response) {
     // var jsonArray=[
     //     {
     //         name:"AAA",
@@ -20,19 +19,27 @@ app.get('/',function(req,response)
     // ];
     // response.json(jsonArray);
     //response.download('./pic.jpg');
-    response.redirect('http://google.com');
-    })
-   
-    //response.end('Pic Download');
-app.listen(27017,function(req,response){
-console.log('response');
+   // response.redirect('http://google.com');
+    response.cookie("name","ratul");
+    response.cookie("profession","engineer");
+    response.send('cookie set successfully');
+})
+//post Request
+app.post('/insertintoheader', function (req, response) {
+    response.append("name","ratul uddin ashraf");
+    response.append("profession","software engineer");
+    response.send('append successfully');
+})
+//response.end('Pic Download');
+app.listen(27017, function (req, response) {
+    console.log('response');
 });
-mongoDb.connect(url, function (error,MyMongoCollection) {
+mongoDb.connect(url, function (error, MyMongoCollection) {
     if (error) {
         console.log("Failed Connection");
     }
     else {
-       // console.log("Connected");
+        // console.log("Connected");
         //insertData(MyMongoCollection);
         //deleteOneMethod(MyMongoCollection);
         //deleteAllData(MyMongoCollection);
@@ -50,28 +57,28 @@ mongoDb.connect(url, function (error,MyMongoCollection) {
 });
 
 function insertData(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
 
     var myobj = { name: "uddin", address: "bd" };
     collection.insertOne(myobj);
 }
 
 function deleteOneMethod(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
 
-    var myobj = {name:"ashraf"};
+    var myobj = { name: "ashraf" };
     collection.deleteOne(myobj);
 
 }
 
 function deleteAllData(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
 
-    collection.deleteMany(function(error,ResultObj) {
-        if(error) {
+    collection.deleteMany(function (error, ResultObj) {
+        if (error) {
             console.log('====================================');
             console.log('failed delete');
             console.log('====================================');
@@ -79,16 +86,16 @@ function deleteAllData(MyMongoCollection) {
         else {
             console.log(ResultObj);
         }
-    }); 
+    });
 
 }
 
 function fineOneWithoutCondition(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
-    var findOneObj={};
-    collection.findOne(findOneObj,function(error,ResultObj) {
-        if(error) {
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
+    var findOneObj = {};
+    collection.findOne(findOneObj, function (error, ResultObj) {
+        if (error) {
             console.log('====================================');
             console.log('failed');
             console.log('====================================');
@@ -96,17 +103,17 @@ function fineOneWithoutCondition(MyMongoCollection) {
         else {
             console.log(ResultObj);
         }
-    }); 
+    });
 
 }
 
 
 function fineOneWithCondition(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
-    var findOneObj={name:"shourov"};
-    collection.findOne(findOneObj,function(error,ResultObj) {
-        if(error) {
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
+    var findOneObj = { name: "shourov" };
+    collection.findOne(findOneObj, function (error, ResultObj) {
+        if (error) {
             console.log('====================================');
             console.log('failed');
             console.log('====================================');
@@ -114,16 +121,16 @@ function fineOneWithCondition(MyMongoCollection) {
         else {
             console.log(ResultObj);
         }
-    }); 
+    });
 
 }
 
 
 function findAll(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
-    collection.find().toArray(function(error,ResultObj) {
-        if(error) {
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
+    collection.find().toArray(function (error, ResultObj) {
+        if (error) {
             console.log('====================================');
             console.log('failed');
             console.log('====================================');
@@ -131,17 +138,17 @@ function findAll(MyMongoCollection) {
         else {
             console.log(ResultObj);
         }
-    }); 
+    });
 
 }
 
 function findAllByProjection(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
 
-    var allColumnObj={};
-    var columnName ={'name':''}
-    collection.find(allColumnObj,columnName).toArray(function(error,result){
+    var allColumnObj = {};
+    var columnName = { 'name': '' }
+    collection.find(allColumnObj, columnName).toArray(function (error, result) {
         console.log('====================================');
         console.log(result);
         console.log('====================================');
@@ -151,19 +158,19 @@ function findAllByProjection(MyMongoCollection) {
 
 
 function findByCondition(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
-    var dataObj ={name:'shourov',address:'bd'}
-    collection.find(dataObj).toArray(function(error,result){
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
+    var dataObj = { name: 'shourov', address: 'bd' }
+    collection.find(dataObj).toArray(function (error, result) {
         console.log(result);
     });
 
 }
 
 function findByConditionWithLimit(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
-    collection.find().limit(1).toArray(function(error,result){
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
+    collection.find().limit(1).toArray(function (error, result) {
         console.log(result);
     });
 
@@ -171,39 +178,39 @@ function findByConditionWithLimit(MyMongoCollection) {
 
 
 function sortData(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
     //1 sort assending order -1 desending order
-    var sortPattern={name:1}
-    collection.find().sort(sortPattern).toArray(function(error,result){
+    var sortPattern = { name: 1 }
+    collection.find().sort(sortPattern).toArray(function (error, result) {
         console.log(result);
     });
 
 }
 
 function UpdateData(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    var collection=database.collection('students');
+    var database = MyMongoCollection.db('school');
+    var collection = database.collection('students');
     //1 sort assending order -1 desending order
-    var objectQuery={name:'shourov',address:'dhaka'}
-    var updateData={$set:{name:'Md.Ratul Uddin Ashraf'}}
-    collection.updateOne(objectQuery,updateData,function(error,result){
+    var objectQuery = { name: 'shourov', address: 'dhaka' }
+    var updateData = { $set: { name: 'Md.Ratul Uddin Ashraf' } }
+    collection.updateOne(objectQuery, updateData, function (error, result) {
         console.log(result);
     });
 
 }
 
 function createCollection(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    database.createCollection('teachers',function(error,result){
+    var database = MyMongoCollection.db('school');
+    database.createCollection('teachers', function (error, result) {
         console.log(result);
     });
 
 }
 
 function deleteCollection(MyMongoCollection) {
-    var database=MyMongoCollection.db('school');
-    database.dropCollection('teachers',function(error,result){
+    var database = MyMongoCollection.db('school');
+    database.dropCollection('teachers', function (error, result) {
         console.log(result);
     });
 
